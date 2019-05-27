329191fe84f100eaa9a84190b2c89571d822a84c
ECWireless <Elliott@valtechcreative.com>
2019-05-27 13:24:45 -0400

    Banner started

--- src/App.js
+++ src/App.js
@@ -15,7 +15,29 @@ function App() {
 					<img className="join" src={Join} alt="Join"/>
 				</div>
 			</div>
-			<div className="banner">Banner</div>
+			<div className="banner">
+				<div className="banner__photo"/>
+				<div className="banner__video">
+					<iframe
+						className="video-size"
+						src="https://player.vimeo.com/video/338717235"
+						frameborder="0"
+						allow="autoplay; fullscreen"
+						allowfullscreen
+						title="banner-video"
+					>
+					</iframe>
+				</div>
+				<div className="button-container">
+					<a href="https://www.wdwright.com/apply-online">
+						<button
+							className="apply-now"
+						>
+							Apply Now!
+						</button>
+					</a>
+				</div>
+			</div>
 			<div className="main">Main</div>
 			<div className="footer">Footer</div>
 		</div>
