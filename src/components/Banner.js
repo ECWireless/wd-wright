69e2c99b4fc507f2ec8452db783f016539bb006c
unknown <Elliott@valtechcreative.com>
2019-06-09 15:10:25 -0400

    Spacing adjustments

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,11 +2,15 @@ import React, { Component, Fragment } from 'react';
 
 export default class Banner extends Component {
 
+	state = {
+		myRef: null,
+	}
+
 	handleScroll = () => {
-		window.scroll({
-			top: 500,
+		window.scrollTo({
+			top: this.myRef.offsetTop,
 			behavior: 'smooth'
-		})
+		});
 	}
 
 	render() {
@@ -34,14 +38,15 @@ export default class Banner extends Component {
 							</button>
 						</a>
 					</div>
-					<div className="learn-more" onClick={this.handleScroll}>
-						<div className="learn-more__text">
+					<div className="learn-more">
+						<div className="learn-more__text" onClick={this.handleScroll}>
 							Learn More
 						</div>
-						<div className="learn-more__symbol">
+						<div className="learn-more__symbol" onClick={this.handleScroll}>
 							&#10093;
 						</div>
 					</div>
+					<div style={{position: 'relative', top: '-40px'}} ref={ (ref) => this.myRef=ref }></div>
 				</div>
 			</Fragment>
 		)
