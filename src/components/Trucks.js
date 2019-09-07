32db30f4e1adb06d5156afff74b7ef636e0b590b
StudioME Admin <StudioMEAdmin@StudioMEs-Edit-Station-2.local>
2019-09-07 11:46:01 -0400

    Truck Section

--- src/components/Trucks.js
+++ src/components/Trucks.js
@@ -4,7 +4,11 @@ export default function Trucks() {
     return (
         <div className="trucks">
             <h1 className="trucks__heading trucks__heading--1">Trucks are high-tech!</h1>
-            <div className="trucks__photos">(Truck Photos)</div>
+            <div className="trucks__photos">
+                <div className="trucks__photo-1"></div>
+                <div className="trucks__photo-2"></div>
+                <div className="trucks__photo-3"></div>
+            </div>
             <h3 className="trucks__heading trucks__heading--2">Have you seen the Wright trucks? You’ll love to drive the new Silverado double cab which features all kinds of upgrades.</h3>
             <ul className="trucks__list">
                 <div>
@@ -15,11 +19,14 @@ export default function Trucks() {
                 </div>
                 
             </ul>
-            <h3 className="trucks__heading trucks__heading--1">Safety is the goal!</h3>
-            <p className="trucks__paragraph">Each unit also has reflective material on hood and back end of the truck, halogen daytime running lights for greater visibility plus speed limit controls.</p>
-            <p className="trucks__paragraph">Plus all the standard features: airbags, rear back-up camera, power-heated outside mirrors.</p>
-            <h3 className="trucks__heading trucks__heading--2">You can earn the right to be a WRIGHT crew leader and drive this truck home at night.</h3>
-            <h3 className="trucks__heading trucks__heading--2">Plus earn safe driver bonus rewards—up to $650 a year.  Real money!!</h3>
+            <div className="trucks__safety">
+                <h3 className="trucks__heading trucks__heading--5">Safety is the goal!</h3>
+                <p className="trucks__paragraph">Each unit also has reflective material on hood and back end of the truck, halogen daytime running lights for greater visibility plus speed limit controls.</p>
+                <p className="trucks__paragraph">Plus all the standard features: airbags, rear back-up camera, power-heated outside mirrors.</p>
+            </div>
+            <h3 className="trucks__heading trucks__heading--3">You can earn the right to be a WRIGHT crew leader and drive this truck home at night.</h3>
+            <div className="trucks__divider"></div>
+            <h3 className="trucks__heading trucks__heading--4">Plus earn safe driver bonus rewards—up to $650 a year.  Real money!!</h3>
             <div className="trucks__bottom"></div>
         </div>
     )
